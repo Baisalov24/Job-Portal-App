c8111797f4e8a14348fb0545cfedafeb5d68e19b
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-14 19:58:40 -0600

    Add user routes API

---
+++ routes/userRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import User from "../models/UserModel.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+export default router;
