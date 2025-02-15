9b3993ad86bb2808e2998bd13119311e69823f5f
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-15 03:51:39 -0600

    feat: add post job page

---
+++ client/src/app/post/page.tsx
@@ -0,0 +1,9 @@
+export default function PostJobPage() {
+    return (
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Post a Job</h1>
+        <p>Fill out the form below to post a new job listing.</p>
+      </div>
+    );
+  }
+  
