62c18879e2c9db0471b967c5d1a7f71554db6d36
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-15 04:20:52 -0600

    fix: display JobForm on PostJobPage

--- client/src/app/post/page.tsx
+++ client/src/app/post/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+import JobForm from "@/components/JobPost/JobForm";
+
 export default function PostJobPage() {
-    return (
-      <div className="p-6">
-        <h1 className="text-2xl font-bold">Post a Job</h1>
-        <p>Fill out the form below to post a new job listing.</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Post a Job</h1>
+      <JobForm />
+    </div>
+  );
+}
