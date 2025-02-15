0cdfbd52bbba5890883343956bc5e75ea03663cd
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-14 21:57:57 -0600

    Add Auth0 authentication

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./db/connect");
+const { auth } = require("express-openid-connect");
 
 dotenv.config();
 const app = express();
@@ -11,11 +12,22 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+app.use(auth(config));
+
 const logger = require("./middleware/loggerMiddleware");
 const errorHandler = require("./middleware/errorMiddleware");
 
 app.use(logger);
-app.use("/api/users", userRoutes);
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
