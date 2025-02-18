e9a7f9c69eaebfbacfe4fb22b8e2a21eb73520af
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-18 14:24:25 -0600

    Modified contents of models folder

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,10 +1,65 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  role: { type: String, enum: ["employer", "job-seeker"], required: true },
-}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-module.exports = mongoose.model("User", UserSchema);
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    auth0Id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    appliedJobs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
+
+    savedJobs: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+      },
+    ],
+
+    role: {
+      type: String,
+      enum: ["jobseeker", "recruiter"],
+      default: "jobseeker",
+    },
+
+    resume: {
+      type: String,
+    },
+
+    profilePicture: {
+      type: String,
+    },
+
+    bio: {
+      type: String,
+      default: "No bio provided",
+    },
+
+    profession: {
+      type: String,
+      default: "Unemployed",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
