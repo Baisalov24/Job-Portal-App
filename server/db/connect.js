be2b018cdce615edfa8c4476516ea753c4641782
Tima Baisalov <baisalov.t98@gmail.com>
2025-02-18 14:19:50 -0600

    Updated database connection settings in connect.js

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+const connect = async () => {
   try {
-    console.log("Connecting to MongoDB...");
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (err) {
-    console.error("Database connection failed:", err);
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("Connected to Database!");
+  } catch (error) {
+    console.log("Failed to connect to Database!", error.message);
     process.exit(1);
   }
 };
 
-export default connectDB;
+export default connect;
